fd942bcbe9eaf2d5305e94878f9206afa8e18312
nouha touati <nouhaila.ribery@gmail.com>
2024-11-19 16:04:02 +0100

    exercice facile

---
+++ Ex_Facile.js
@@ -0,0 +1,56 @@
+// exercice 1
+const numbers=[1,2,3,4,5]
+console.log( numbers)
+// EXERCICE 2
+const fruits=["apple","banana","cherry"]
+console.log(fruits[1])
+// exercice 3
+const numb=[10,20,30,40]
+numb[2]=50
+console.log(numb)
+// Exercice 4
+const fruits1=["mango","peach","pear"]
+fruits1[3]="krimisa",
+console.log(fruits1)
+// exercice 5
+const animal=["dog","cat","bird"]
+delete animal[2]
+animal.pop()
+console.log(animal)
+
+// exercice 6
+const personne={
+    name:"Nouhaila",
+    age:22,
+}
+console.log("le nom est "+personne.name)
+console.log("l'age est "+personne.age)
+// EX 7
+const pers={
+    name:"john ",
+    age:25
+}
+console.log("le nom est"+pers.name)
+console.log("l'age"+pers.age)
+// exercice 8
+const book={
+    title:"JS 101",
+    author:"JOHN DOE",
+}
+book.author="jane doe"
+console.log(book)
+// exercice 9
+const car={
+    model:"corolla",
+    make:"Toyota",
+    year:2020
+}
+console.log(car)
+// exercice 10
+const p={
+    name:"nouha",
+    age:22
+
+}
+const Hasage="P" in p
+console.log(Hasage)
